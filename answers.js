d5c323b989e6355c5af71af6ac909e9f68aec50a
noura17-meet <noura17@meet.mit.edu>
2016-11-27 19:55:49 +0200

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,10 +1,28 @@
-///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
+
 
 ///1. VARIABLES
+var temperature= 98.6
+temoerature= temperature+5;
+temperature= temperature*3;
+temperature= temperature-20;
+console.log(temperature);
+alert(temperature);
 
 ///2. DATA STRUCTURES
 
+var Days_of_the_Week = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" , "Saturday"];
+Days_of_the_Week[0] = "domingo";
+Days_of_the_Week[1] = "lunes";
+Days_of_the_Week[2] = "martes";
+Days_of_the_Week[3] = "miercoles";
+Days_of_the_Week[4] = " Jueves";
+Days_of_the_Week[5] = "viernes";
+Days_of_the_Week[6] = "sabado";
+
+console.log(Days_of_the_Week[0])icecream_flavours={"Vanilla":"$1.99" , "Chocolate":"$2.49", "Strawberry":"$2.25" };
+console.log(icecream_flavours["Vanilla"]);
 ///3. FUNCTIONS & CONDITIONAL STATEMENTS
+
 
 ///4.  LOOPS
 
